acb54a7a2241b7a472047626c5ed7054a5913bcc
idpan <idpanashari813@gmail.com>
2026-08-28 23:54:51 +0700

    --feat: add portofolio empty state (closes #10)

--- src/pages/Portofolio.tsx
+++ src/pages/Portofolio.tsx
@@ -1,40 +1,53 @@
 import { getPortofolio } from "../hooks/usePortofolio";
-import { coinData } from "../data/coinData"; // sesuaikan path
-
+import { coinData } from "../data/coins";
+import { Link } from "react-router";
 export default function Portofolio() {
   const { holdings } = getPortofolio();
 
   return (
     <>
       <h1>Portofolio</h1>
-      {holdings.map((holding) => {
-        const coin = coinData.find((c) => c.id === holding.coinId);
 
-        if (!coin) return null; // jaga-jaga kalau coinId gak ketemu
+      {!holdings[0] ? (
+        <>
+          <h2>Belum ada holdings</h2>
+          <p className="mb-10">
+            Beli koin di halaman Market untuk mulai membangun portofolio
+          </p>
+          <Link to="/market" className="bg-black text-white p-5 rounded-lg">
+            Ke halaman Market
+          </Link>
+        </>
+      ) : (
+        holdings?.map((holding) => {
+          const coin = coinData.find((c) => c.id === holding.coinId);
 
-        return (
-          <div
-            key={holding.coinId}
-            className="border border-black w-96 grid grid-cols-4"
-          >
-            <div>
-              <img
-                src={`/logos/${coin.logo}`}
-                alt={coin.name}
-                className="w-8 h-8"
-              />
+          if (!coin) return null; // jaga-jaga kalau coinId gak ketemu
+
+          return (
+            <div
+              key={holding.coinId}
+              className="border border-black w-96 grid grid-cols-4"
+            >
+              <div>
+                <img
+                  src={`/logos/${coin.logo}`}
+                  alt={coin.name}
+                  className="w-8 h-8"
+                />
+              </div>
+              <div>
+                <p className="font-bold">{coin.name}</p>
+                <p className="text-gray-400">{coin.ticker}</p>
+              </div>
+              <div>
+                <p>{holding.qty}</p>
+                <p className="text-xl">{holding.avgPrice}</p>
+              </div>
             </div>
-            <div>
-              <p className="font-bold">{coin.name}</p>
-              <p className="text-gray-400">{coin.ticker}</p>
-            </div>
-            <div>
-              <p>{holding.qty}</p>
-              <p className="text-xl">{holding.avgPrice}</p>
-            </div>
-          </div>
-        );
-      })}
+          );
+        })
+      )}
     </>
   );
 }
